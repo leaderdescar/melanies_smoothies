ecf7067a151e729dcf336c88d76ab3eb531d83ad
Chris Ehmett <stanlumber@gmail.com>
2025-11-24 15:16:32 -0500

    Update streamlit_app.py
    data frame and json formatting api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-smoothies_response = requests.get("https://my.smothiesfroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smothiesfroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw: {st.__version__}")
